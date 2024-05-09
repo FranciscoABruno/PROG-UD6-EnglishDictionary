2072f6bd5c83c5210790cfedb62d65e9053c4cf2
FranciscoABruno <usr3760@salesianos-lacuesta.net>
2024-05-09 13:32:35 +0100

    feat : hashmap clase Diccionario

--- src/com/duolingo/entities/Diccionario.java
+++ src/com/duolingo/entities/Diccionario.java
@@ -1,8 +1,13 @@
 package com.duolingo.entities;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
 public class Diccionario {
     private Map<Character, Set<String>> palabrasPorInicial;
+
+        public Diccionario() {
+        palabrasPorInicial = new HashMap<>();
+    }
 }
