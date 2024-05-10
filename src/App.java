cb39fe65158ef74c97181b8d72ea51423d5e6e81
FranciscoABruno <usr3760@salesianos-lacuesta.net>
2024-05-10 12:45:37 +0100

    feat : clase App

--- src/App.java
+++ src/App.java
@@ -9,14 +9,14 @@ public class App {
 
         boolean salir = false;
         while (!salir) {
-            System.out.println("\nMenú:");
+            System.out.println("\nMenú: ");
             System.out.println("1. Añadir palabra");
             System.out.println("2. Eliminar palabra");
             System.out.println("3. Existe palabra");
             System.out.println("4. Mostrar iniciales disponibles");
             System.out.println("5. Ver palabras por inicial");
             System.out.println("6. Cerrar programa");
-            int opcion = InputUtils.leerEntero("Seleccione una opción: ");
+            int opcion = InputUtils.readInt("Selecciona una opción: \n");
 
             switch (opcion) {
                 case 1:
