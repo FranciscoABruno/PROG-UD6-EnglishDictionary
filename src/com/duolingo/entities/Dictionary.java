c7375c433738eda6e08d5ff97327c0e01de04f1c
FranciscoABruno <usr3760@salesianos-lacuesta.net>
2024-05-10 12:39:09 +0100

    feat : clase Dictionary

--- src/com/duolingo/entities/Dictionary.java
+++ src/com/duolingo/entities/Dictionary.java
@@ -1,14 +1,46 @@
 package com.duolingo.entities;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
+import com.duolingo.utilities.StringUtils;
+
 public class Dictionary {
-    private Map<Character, Set<String>> palabrasPorInicial;
+    private Map<Character, Set<String>> wordsByInitial;
 
     public Dictionary() {
-        palabrasPorInicial = new HashMap<>();
+        wordsByInitial = new HashMap<>();
     }
 
+    public void addWord(String word) {
+        word = StringUtils.sanitize(word);
+        char initial = word.charAt(0);
+        wordsByInitial.putIfAbsent(initial, new HashSet<>());
+        wordsByInitial.get(initial).add(word);
+    }
+
+    public boolean removeWord(String word) {
+        word = StringUtils.sanitize(word);
+        char initial = word.charAt(0);
+        if (wordsByInitial.containsKey(initial)) {
+            return wordsByInitial.get(initial).remove(word);
+        }
+        return false;
+    }
+
+    public boolean hasWord(String word) {
+        word = StringUtils.sanitize(word);
+        char initial = word.charAt(0);
+        return wordsByInitial.containsKey(initial) && wordsByInitial.get(initial).contains(word);
+    }
+
+    public Set<Character> getAvailableInitials() {
+        return wordsByInitial.keySet();
+    }
+
+    public Set<String> getWordsByInitial(char initial) {
+        return wordsByInitial.getOrDefault(initial, new HashSet<>());
+    }
 }
--- src/com/duolingo/utilities/StringUtils.java
+++ src/com/duolingo/utilities/StringUtils.java
@@ -1,5 +1,8 @@
 package com.duolingo.utilities;
 
 public class StringUtils {
-
+    public static String sanitize(String word) {
+        // Quita los espacios iniciales y finales y convierte a minúsculas
+        return word.trim().toLowerCase();
+    }
 }
