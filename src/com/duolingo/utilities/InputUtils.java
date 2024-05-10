49ca684e3b3fb2f89fe1746d58580ff64badcb7d
FranciscoABruno <usr3760@salesianos-lacuesta.net>
2024-05-10 12:40:57 +0100

    feat : clase InputUtils

--- src/com/duolingo/utilities/InputUtils.java
+++ src/com/duolingo/utilities/InputUtils.java
@@ -1,9 +1,17 @@
 package com.duolingo.utilities;
 
-public class InputUtils {
+import java.util.Scanner;
 
-    public static int leerEntero(String string) {
-        
+public class InputUtils {
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static int readInt(String message) {
+        System.out.print(message);
+        while (!scanner.hasNextInt()) {
+            System.out.println("Ingresa un número entero válido");
+            System.out.print(message);
+            scanner.next();
+        }
+        return scanner.nextInt();
     }
-    
 }
